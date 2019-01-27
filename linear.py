306b1d6900fed51927da2f534e215a55c19055a9
howon lee <hleehowon@gmail.com>
2019-01-27 08:08:13 -0800

    add linear

---
+++ linear.py
@@ -0,0 +1,56 @@
+import numpy as np
+import numpy.random as npr
+import numpy.linalg as npl
+
+if __name__ == "__main__":
+    npr.seed(1337)
+    """
+    We have rank issues if we don't have these square
+    or at least proper row rank, unfortunately.
+
+    This is a test of the optimization methods, so apologies for
+    not having actual data
+    """
+    xs = npr.randn(100, 100)
+    ys = npr.randn(100, 100)
+    """
+    Cache the original unlearned theta so we can see
+    how the two algos do on the same minibatch
+    """
+    original_theta = npr.randn(100, 100)
+    print("training with gradient descent...")
+    alpha = 1e-3
+    theta = original_theta
+    for it in range(50000):
+        est = np.dot(xs, theta)
+        err = 0.5 * np.sum(np.power(est - ys, 2))
+        if it % 500 == 0:
+            print("it: {}, err: {}".format(str(it), str(err)))
+        # derr_dest as in, derivative of error wrt estimate
+        derr_dest = est - ys
+        derr_dtheta = np.dot(xs.T, derr_dest)
+        theta -= alpha * derr_dtheta
+    print("resetting theta and redoing with finite difference Newton's...")
+    # note we can get away w/ less relaxed relaxation of course
+    alpha = 1e-1
+    r = 1e-8
+    theta = original_theta
+    for it in range(500):
+        est = np.dot(xs, theta)
+        err = 0.5 * np.sum(np.power(est - ys, 2))
+        if it % 50 == 0:
+            print("it: {}, err: {}".format(str(it), str(err)))
+        # derr_dest as in, derivative of error wrt estimate
+        derr_dest = est - ys
+        derr_dtheta = np.dot(xs.T, derr_dest)
+        """
+        fd stands for finite difference
+        doing one side only fd like a bad person (for demo only)
+        v is derr_dtheta itself, so (derr_dtheta + r * derr_dtheta)
+        """
+        fd_derr_dtheta = derr_dtheta * (1. + r)
+        fd_derr_dest = np.dot(npl.inv(xs.T), fd_derr_dtheta)
+        fd_est = fd_derr_dest + ys
+        # executing the diff here
+        fd_theta = (np.dot(npl.inv(xs), fd_est) - theta) / r
+        theta -= alpha * fd_theta
---
+++ readme.md
@@ -0,0 +1,16 @@
+J. R. "Bob" Dobbs Memorial Fast Finite Difference Newton's Method
+===
+
+or, a fun little demo of the J. R. "Bob" Dobbs Memorial Inverse Hessian Multiplication Method.
+
+Of course, you can get a closed-form solution to linear regression, but if you want to you can also do gradient descent. Rosenbrock, you need an optimization thing. Works as well as the "slow" Hessian inversion method, which of course doesn't make a real difference with the Rosenbrock function.
+
+I present a method to do Newton's method on the same order as gradient descent (assuming minibatches of same size as the weights, as there's a matrix inversion involved: otherwise, of the order of the matrix inversion only). No bias term, because I didn't feel like it. You can always simulate by mutating the x matrix.
+
+Both gradient of linear regression and the Rosenbrock function, it turns out, is amenable to easy functional inverse, so you can get multiplication of inverse Hessians with gradient really easily according to the [method I found](https://github.com/howonlee/bobdobbshess). So I did, and as you can see if you run it, the thing works gangbusters. If you don't understand that method you're not going to understand this repo.
+
+If you need to learn what regression is in 10 different ways, I recommend _Elements of Statistical Learning_ although I recognize that's a perverse and stupid recommendation and you should take a statistics 101 class or something.
+
+This is continuing on the work I did in the CSPAM thread and you'd probably be best off asking question or making comments there, although I'll probably end up hanging in the HN thread for a while.
+
+I'm still working on doing it for deep nets. There, functional inversion is not easy and is not fun times, numerically.

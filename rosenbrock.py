430f78a0dab72cd8ed4c9b3f77e609aae0e0e2ca
howon lee <hleehowon@gmail.com>
2019-01-29 11:21:55 -0800

    ok, give it the testing

--- rosenbrock.py
+++ rosenbrock.py
@@ -10,12 +10,36 @@ def rosenbrock_grad(x, y):
     return z1, z2
 
 def rosenbrock_inv_grad(z1, z2):
+    """
+    Gotten by a paper and pencil, unfortunately
+    """
     x = (z1 + 2.) / (2. - (2. * z2))
     y = (z2 / 200.) + (((z1 ** 2) + (4. * z1) + 4.) / (4. - (8. * z2) + (4. * (z2 ** 2))))
     return x, y
 
 if __name__ == "__main__":
-    x, y = npr.randn(), npr.randn()
-    print(x, y)
-    print(rosenbrock_grad(x, y))
-    print(rosenbrock_inv_grad(*rosenbrock_grad(x, y)))
+    orig_x, orig_y = npr.randn(), npr.randn()
+    print("original x and y: ({}, {})".format(orig_x, orig_y))
+    iters = 20
+    curr_x, curr_y = orig_x, orig_y
+    print("lame gradient method, bounces around a lot")
+    alpha = 1e-3
+    for it in range(iters):
+        val = rosenbrock(curr_x, curr_y)
+        print("iter: {}, current val of rosenbrock: {}".format(str(it), str(val)))
+        z1, z2 = rosenbrock_grad(curr_x, curr_y)
+        curr_x -= alpha * z1
+        curr_y -= alpha * z2
+    r = 1e-8
+    print("final (x,y) with gradient descent: ({}, {})".format(curr_x, curr_y))
+    print("finite difference newton's method: look ma, no explicit Hessian!")
+    print("resetting x and y back to original values...")
+    curr_x, curr_y = orig_x, orig_y
+    for it in range(iters):
+        val = rosenbrock(curr_x, curr_y)
+        print("iter: {}, current val of rosenbrock: {}".format(str(it), str(val)))
+        z1, z2 = rosenbrock_grad(curr_x, curr_y)
+        fd_x, fd_y = rosenbrock_inv_grad(z1 * (1. + r), z2 * (1. + r))
+        curr_x -= (fd_x - curr_x) / r
+        curr_y -= (fd_y - curr_y) / r
+    print("final (x,y) with newton's method: ({}, {})".format(curr_x, curr_y))
